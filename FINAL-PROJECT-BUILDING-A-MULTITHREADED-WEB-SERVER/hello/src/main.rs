1b9f77ef868f28aef30702f849902a914ccf8189
Animesh Raj <animesh080808@gmail.com>
2025-09-02 07:55:44 +0530

    We are ending this rust book with multi threaded web server

--- FINAL-PROJECT-BUILDING-A-MULTITHREADED-WEB-SERVER/hello/src/main.rs
+++ FINAL-PROJECT-BUILDING-A-MULTITHREADED-WEB-SERVER/hello/src/main.rs
@@ -2,15 +2,22 @@ use std::{
     fs,
     io::{prelude::*, BufReader}, 
     net::{TcpListener, TcpStream}, 
+    thread, 
+    time::Duration,
 }; 
+
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); 
-
+    let pool = ThreadPool::new(4); 
     for stream in listener.incoming() {
         let stream = stream.unwrap(); 
 
-        handle_connection(stream); 
+        pool.execute(|| { 
+            println!("-----------------{:#?}",stream);
+            handle_connection(stream); 
+        });
     }
 } 
 
@@ -22,24 +29,23 @@ fn handle_connection(mut stream: TcpStream) {
         .unwrap() 
         .unwrap(); 
 
-    if request_line == "GET / HTTP/1.1" { 
-        let status_line = "HTTP/1.1 200 OK"; 
-
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect(); 
- 
-    let status_line = "HTTP/1.1 200 OK"; 
-    let contents = fs::read_to_string("hello.xhtml").unwrap(); 
+    let (status_line, filename) = match &request_line[..] { 
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.xhtml"), 
+        "GET /sleep HTTP/1.1" => { 
+            thread::sleep(Duration::from_secs(5)); 
+            ("HTTP/1.1 200 OK", "hello.xhtml") 
+        } 
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.xhtml"), 
+    }; 
+    let contents = fs::read_to_string(filename).unwrap(); 
     let length = contents.len(); 
- 
-   let response = format!( 
-        r"{status_line}\r\n\ 
+
+    let response = format!( 
+    r"{status_line}\r\n\ 
          Content-Length: {length}\r\n\r\n\ 
          {contents}" 
-    ); 
- 
-    stream.write_all(response.as_bytes()).unwrap();
+); 
+
+    stream.write_all(response.as_bytes()).unwrap(); 
+
 }
