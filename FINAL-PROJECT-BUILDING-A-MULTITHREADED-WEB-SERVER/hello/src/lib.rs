347a16ea7ebdebe641924f3e957c5aac20210197
Animesh Raj <animesh080808@gmail.com>
2025-09-21 00:00:53 +0530

    temp

--- FINAL-PROJECT-BUILDING-A-MULTITHREADED-WEB-SERVER/hello/src/lib.rs
+++ FINAL-PROJECT-BUILDING-A-MULTITHREADED-WEB-SERVER/hello/src/lib.rs
@@ -4,27 +4,35 @@ use std::thread;
 pub struct ThreadPool { 
     threads: Vec<thread::JoinHandle<()>>, 
 } 
+
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
-        let mut threads = Vec::with_capacity(size); 
+        assert!(size > 0); 
+        let mut workers = Vec::with_capacity(size); 
 
-        for _ in 0..size { 
-            // create some threads and store them in the vector 
-        } 
+        for id in 0..size { 
+            workers.push(Worker::new(id)); 
+        }
 
-        ThreadPool { threads } 
+        ThreadPool { workers } 
     }
-    pub fn execute<F>(&self, f: F) 
+}
+
+struct Worker { 
+    id: usize, 
+    thread: thread::JoinHandle<()>, 
+} 
+
+impl Worker { 
+    fn new(id: usize) -> Worker { 
+        let thread = thread::spawn(|| {}); 
+
+        Worker { id,thread } 
+    } 
+}
+
+pub fn execute<F>(&self, f: F) 
 where 
-        F: FnOnce() + Send + 'static, 
-    { 
-    }
-    pub fn build( 
-        size: usize 
-    ) -> Result<ThreadPool, PoolCreationError> {
-
-
-    }
-
+    F: FnOnce() + Send + 'static, 
+{ 
 }
